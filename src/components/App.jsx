9f140088c585ac4dab429293dff548c260e1b6d1
IvSem <livansll96@gmail.com>
2023-05-24 14:07:40 +0300

    initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,17 @@
+export const App = () => {
+	return (
+		<div
+			style={{
+				height: '100vh',
+				display: 'flex',
+				justifyContent: 'center',
+				alignItems: 'center',
+				fontSize: 40,
+				backgroundColor: 'blue',
+				color: 'burlywood',
+			}}
+		>
+			React homework template
+		</div>
+	);
+};
